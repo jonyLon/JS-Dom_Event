62cba3eadcc22fd51e88c0eed14990575004bb16
jonyLon <masterlordgoker@gmail.com>
2024-05-31 17:51:15 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,47 @@
+let item = document.querySelector("#inputer");
+item.addEventListener("keypress", (e) => {
+  if (e.key) {
+    e.preventDefault();
+  }
+});
+
+
+let getRandom = () => Math.floor(Math.random() * 256);
+
+let getRGB = () => `rgb(${getRandom()},${getRandom()},${getRandom()})`
+
+
+function setElementToBox() {
+  let div = document.createElement('div')
+  div.style.width = div.style.height = '100px'
+  let rgb = getRGB();
+  div.style.backgroundColor = rgb;
+  // div.style.display = 'flex';
+  // div.style.justifyContent = 'center';
+  // div.style.alignItems = 'center';
+  // let p = document.createElement('p')
+  // p.textContent = rgb;
+  // div.append(p)
+  //document.querySelector('.root').append(div)
+  // if (!flag)
+  //   document.querySelector('.root').insertAdjacentElement('afterbegin', div)
+  // else
+  //   document.querySelector('.root').insertAdjacentElement('beforeend', div)
+  return div
+}
+
+document.querySelectorAll('button')[0].addEventListener('click', () => {
+  document.querySelector('#box').appendChild(setElementToBox())
+})
+
+
+document.querySelectorAll('.color-cell').forEach(cell => {
+  cell.addEventListener('click', () => {
+    const color = cell.getAttribute('data-color');
+    document.getElementById('text').style.color = color;
+  })
+})
+
+
+
+
